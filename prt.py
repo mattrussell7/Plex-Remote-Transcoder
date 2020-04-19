c856c6bc6151ae22cf4a7de9f95bd6ebd2fd87b0
mattrussell7 <52579751+mattrussell7@users.noreply.github.com>
2020-04-19 20:17:52 +0800

    fixed indenting

--- prt.py
+++ prt.py
@@ -258,7 +258,7 @@ def build_env(host=None):
     if backslashcheck is not None:
         ffmpeg_path_fixed = ffmpeg_path.replace('\\','')
         os.environ["FFMPEG_EXTERNAL_LIBS"] = str(ffmpeg_path_fixed)
-		for k, v in ENV_VARS.items():
+    for k, v in ENV_VARS.items():
         os.environ[k] = v
     envs = ["export %s=%s" % (k, pipes.quote(v)) for k,v in ENV_VARS.items()]
     envs.append("export PRT_ID=%s" % uuid.uuid1().hex)
